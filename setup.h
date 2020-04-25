103fca9b36e848817189c856f57c433f86238f8c
Appuxif <smokost@gmail.com>
2020-04-25 18:59:56 +0700

    init

--- setup.h
+++ setup.h
@@ -1,2 +1,3 @@
 sudo apt-get install -y libglib2.0-0 libnss3 libgconf-2-4  libfontconfig1
 sudo apt-get install -y make git zlib1g-dev libssl-dev gperf php cmake clang-6.0 libc++-dev libc++abi-dev
+sudo apt-get install -y python3-dev libmysqlclient-dev
