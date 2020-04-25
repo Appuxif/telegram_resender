22b82d3c6907d9dc67da310fd6dba87d44f25c82
Appuxif <smokost@gmail.com>
2020-04-25 20:48:36 +0700

    communications added

--- webapp/interface/apps.py
+++ webapp/interface/apps.py
@@ -12,8 +12,8 @@ class InterfaceConfig(AppConfig):
     # Тут запускаем процессор, который управляет запуском клиентов
     def ready(self):
         global processor
-        if processor is None:
-            from .models import TelegramClient
-            processor = Processor(clients=list(TelegramClient.objects.all()))
-            # t = threading.Thread(target=processor.go_processor, daemon=True)
-            # t.start()
+        # if processor is None:
+        #     from .models import TelegramClient
+        #     processor = Processor(clients=list(TelegramClient.objects.all()))
+        #     t = threading.Thread(target=processor.go_processor, daemon=True)
+        #     t.start()
