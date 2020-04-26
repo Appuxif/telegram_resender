7f0f34beb47577d14b1984c76908052ad5ad0928
Appuxif <smokost@gmail.com>
2020-04-27 02:15:32 +0700

    use connections

--- webapp/bot_processor.py
+++ webapp/bot_processor.py
@@ -197,7 +197,7 @@ class Processor:
     def listener_thread(self):
         print('Запуск листенера')
         # with mp.connection.Listener('/home/ubuntu/telegram_resender/webapp/processor.sock', family='AF_UNIX') as listener:
-        with mp.connection.Listener(r'\.\pipePIPENAME', family='AF_UNIX') as listener:
+        with mp.connection.Listener(r'\.\pipePIPENAME', family='AF_UNIX', authkey=b'asd') as listener:
 
             conn = listener.accept()
             print('connection accepted from', listener.last_accepted)
